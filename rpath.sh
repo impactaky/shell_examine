1df4c4ae3461eb2ccd37fd26fc9e838a5daac90e
impactaly <impactaky@users.noreply.github.com>
2023-12-14 20:56:17 +0900

    First commit

---
+++ rpath.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eu
+
+# RUNPATH=/usr/lib/foo -> RUNPATH=/host/usr/lib/foo
+# RUNPATH=$ORIGIN/foo  -> RUNPATH=$ORIGIN/foo
+rpath () {
+    RUNPATH=$(objdump -x $1 2> /dev/null | awk '$1=="RUNPATH" && !($2~/^\$ORIGIN/) {print $2}')
+    if [[ $RUNPATH ]]; then
+        echo $RUNPATH
+    fi
+    # if [[ $RUNPATH ]]; then
+    #     deploy_bin=./deploy/${1##*/}
+    #     cp $1 $deploy_bin
+    #     echo $RUNPATH \
+    #         | sed -e "s#^/#/host/#" -e "s#:/#:/host/#g" \
+    #         | xargs -I {} /usr/bin/patchelf --set-rpath '{}' $deploy_bin
+    #     echo add /host prefix to RUNPATH in $deploy_bin >> ./target.log
+    # fi
+}
+export -f rpath
+
+find /usr/bin | xargs -P1 -I {} bash -c "rpath {}"
---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -eu
+
+mkdir ./bin/
+cp -r /usr/bin/sudo ./bin/
+cp -r /usr/bin/p* ./bin/
