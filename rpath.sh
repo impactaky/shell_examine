8ad71d1f52e624efc6550cde8c6e3a547e9f97cc
impactaly <impactaky@users.noreply.github.com>
2023-12-25 20:55:31 +0900

    uncomment

--- rpath.sh
+++ rpath.sh
@@ -9,15 +9,15 @@ rpath () {
     if [[ $RUNPATH ]]; then
         echo $RUNPATH
     fi
-    # if [[ $RUNPATH ]]; then
-    #     deploy_bin=./deploy/${1##*/}
-    #     cp $1 $deploy_bin
-    #     echo $RUNPATH \
-    #         | sed -e "s#^/#/host/#" -e "s#:/#:/host/#g" \
-    #         | xargs -I {} /usr/bin/patchelf --set-rpath '{}' $deploy_bin
-    #     echo add /host prefix to RUNPATH in $deploy_bin >> ./target.log
-    # fi
+    if [[ $RUNPATH ]]; then
+        deploy_bin=./deploy/${1##*/}
+        cp $1 $deploy_bin
+        echo $RUNPATH \
+            | sed -e "s#^/#/host/#" -e "s#:/#:/host/#g" \
+            | xargs -I {} /usr/bin/patchelf --set-rpath '{}' $deploy_bin
+        echo add /host prefix to RUNPATH in $deploy_bin >> ./target.log
+    fi
 }
 export -f rpath
 
-find ./bin | xargs -P$(nproc) -I {} bash -c "rpath {}"
+find ./bin | xargs -P`nproc` -I {} bash -c "rpath {}"
